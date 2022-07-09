4f6d9b5fdf87b109f8b2227cefbf284452403853
Na-taKo <mrs.komarova@gmail.com>
2022-07-09 16:49:07 +0300

    Miles

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+        public static void main(String[] args) {
+
+            int ticket = 3_000;
+            int oneMileTicket = 20;
+
+            int miles = ticket / oneMileTicket;
+            System.out.println("Бонусные мили: "+ miles);
+        }
+}
